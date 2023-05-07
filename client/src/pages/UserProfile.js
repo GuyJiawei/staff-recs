b61a6a32c3b75a9b3a14714b022e905470aa74af
Guy J Anderson <88031354+GuyJiawei@users.noreply.github.com>
2023-05-07 11:12:17 +1000

    added userprofile logic

---
+++ client/src/pages/UserProfile.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { GET_USER_PROFILE } from '../utils/mutations';
+import Auth from '../utils/auth';
+
+function UserProfile() {
+  const { userId } = useParams();
+  const { loading, data } = useQuery(GET_USER_PROFILE, {
+    variables: { userId: userId || Auth.getProfile().data._id },
+  });
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  const user = data.user;
+
+  return (
+    <div className="container">
+      <h2>{user.userName}'s Profile</h2>
+      <h3>Genres</h3>
+      <ul>
+        {user.genres.map((genre) => (
+          <li key={genre}>{genre}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default UserProfile;
